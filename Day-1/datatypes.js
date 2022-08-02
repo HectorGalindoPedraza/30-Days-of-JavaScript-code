4bcba642a7388671eb501ac2a6db7f2141dea393
hecgph <hec.gph@proton.me>
2022-08-02 15:25:39 -0500

    Primer dia del reto

---
+++ Day-1/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof(text))
+console.log(typeof(bool))
+console.log(typeof(und))
+console.log(typeof(neh))
---
+++ Day-1/variable.js
@@ -0,0 +1,39 @@
+let text = ''
+let  bool = true
+let bool2 = false
+let und = undefined
+let neh = null
+
+let space1
+let space2
+let space3
+let space4
+
+let val1 = 23
+let val2 = 'Nombre: '
+let val3 = false
+let val4 = 'Last Name: '
+
+
+/* Declare variables to store your first name, last name,
+ marital status, country and age in multiple lines */
+
+let names = 'Héctor'
+let lastName = 'Galindo'
+let maritalStatus = 'single'
+let country = 'Colombia'
+let age = 17
+
+/* Declare variables to store your first name, last name,
+ marital status, country and age in a single line */
+
+let name = 'Hector', apellido = 'Galindo', estadoCivil = 'Soltero', pais = 'Colombia', edad = 17
+
+/* Declare two variables myAge and yourAge and assign them initial
+ values and log to the browser console. */
+
+let myAge = 17
+let yourAge = 16
+
+console.log(`I am ${myAge} years old`)
+console.log(`You are ${yourAge} years old`)
